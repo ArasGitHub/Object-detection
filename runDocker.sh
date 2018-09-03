97ea1497a72d4e9005c8cf782844dacfa6dd09cb
igneous <aras.azimi@gmail.com>
2018-09-03 17:15:45 -0400

    * adding gitignore * setting runDocker.sh for igneous dell2_ubuntu

--- runDocker.sh
+++ runDocker.sh
@@ -1,6 +1,6 @@
-xhost +local:docker
+xhost +local:dockeri
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -m 8GB -it --rm --device=/dev/video0 -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v /home/leo/Documents/SandBox/realTime-ObjectDetection:/lab  realtime-objectdetection
+docker run -m 8GB -it --rm --device=/dev/video0 -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ~/Desktop/ws/gh/Object-detection:/lab rtod
 xhost -local:docker
